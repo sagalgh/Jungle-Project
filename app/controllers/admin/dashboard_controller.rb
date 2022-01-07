e646c1c9610c194c4d6b74f1e1e4626fb94e9948
Sagal Ghelle <sagal.ghelle@gmail.com>
2022-01-07 15:17:30 -0500

    add env file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  before_filter :authenticate
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  before_filter :authenticate
   def index
     @products = Product.order(id: :desc).all
   end
